928b5ae0090adb2cbf38025752c85e06da5ae3db
Etienne Mustow <etienne.o.mustow@gmail.com>
2017-09-11 15:13:40 +0100

    Pictures of cat and Ray added

--- app.rb
+++ app.rb
@@ -11,3 +11,18 @@ end
 get '/fancy' do
 "I'm fancy"
 end
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
+
+get '/ray' do
+  "<div>
+    <img src='http://bit.ly/2xZt6po'>
+    </div>"
+end
+
+
+set :session_secret, 'super secret'
